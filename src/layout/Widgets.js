2dbaa7bade5c344cc713a71374707e2c3a9967f4
yigitcandonmez <muratcandonmez123@gmail.com>
2022-02-06 17:45:56 +0300

    searchBox

--- src/layout/Widgets.js
+++ src/layout/Widgets.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { SearchIcon } from "../icons/Icon";
 
 const Widgets = () => {
-  return <aside className="w-80 text-white">widgets</aside>;
+  return (
+    <aside className="w-80 px-3 text-white">
+      <div className="flex  py-2 px-5 mt-2  rounded-full bg-zinc-800 bg-opacity-90">
+        <SearchIcon />
+        <input
+          type="text"
+          className="w-full bg-transparent outline-none pl-3 placeholder:text-gray-500 placeholder:text-base"
+          placeholder="Search Twitter"
+        ></input>
+      </div>
+    </aside>
+  );
 };
 
 export default Widgets;
